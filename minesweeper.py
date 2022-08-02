1fa50d1bb1105007fbc6e47caca421dde3d21223
Roman Navarro <103530319+CavemanCorps@users.noreply.github.com>
2022-08-01 19:32:00 -0700

    Update minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -1,11 +1,11 @@
 #! python3
 # minesweeper.py - A small minesweeper-esque game
-import random
-import sys
 
 
 # Hashtag board
+
 field = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 
 def layout(board):
     print(str(field[0]) + '|' + str(field[1]) + ' |' + str(field[2]))
@@ -14,7 +14,11 @@ def layout(board):
     print('-+--+-')
     print(str(field[6]) + '|' + str(field[7]) + ' |' + str(field[8]))
 
+
 # Game Logic
+
+import random
+import sys
 while True:
     print('COLLECT ALL THE CASH AND AVOID STEPPING ON THE MINE')
     nul = '$'
@@ -30,4 +34,6 @@ while True:
             layout(field)
             print('KABOOM')
             sys.exit()
-    print('YOU HAVE SUCCESSFULLY CLEARED THE FIELD')
+
+    print('YOU HAVE SUCCESSFULLY CLEARED THE FIELD.')
+    sys.exit()
